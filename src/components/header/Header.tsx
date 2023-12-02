dc726bfd7650bd2139108d6662fa66babb9f3ac7
Bri <brianjcarroll0@gmail.com>
2023-12-02 16:44:32 -0500

    adds files and deps

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,38 @@
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback,
+} from "../../components/ui/avatar";
+import { useId } from "react";
+
+export default function Header() {
+  const RANDOM_SEED = useId();
+  // Seed should be some stored user ID
+  const seed = null;
+  const BOT_AVATAR_ENDPOINT = `https://api.dicebear.com/7.x/bottts-neutral/svg?seed=${
+    seed || RANDOM_SEED
+  }`;
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-8">
+        {/* <TeamSwitcher /> */}
+        {/* <MainNav className="mx-6" /> */}
+        <div className="flex w-full items-center space-x-4">
+          <div className="flex w-full items-center justify-between gap-x-2">
+            <img
+              className="h-8"
+              src="https://user-images.githubusercontent.com/81318863/280472537-02cdaaa6-493c-4b58-805d-f86fd449f71d.png"
+              alt="resolvr"
+            />
+          </div>
+          <Avatar>
+            <AvatarImage src={BOT_AVATAR_ENDPOINT} />
+            <AvatarFallback>Avatar</AvatarFallback>
+          </Avatar>
+          {/* <Search /> */}
+          {/* <UserNav /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
